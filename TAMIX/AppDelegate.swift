34c07a79c001baf65c8d3915c8b70562d6e729f0
Sergio Daniel Lozano <sergio.lozano@grability.com>
2016-06-07 15:11:35 -0500

    Initial Commit

---
+++ TAMIX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TAMIX
+//
+//  Created by Sergio Daniel Lozano on 6/7/16.
+//  Copyright © 2016 Sergio Daniel L Garcia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TAMIX/Document.swift
@@ -0,0 +1,29 @@
+//
+//  Document.swift
+//  TAMIX
+//
+//  Created by Sergio Daniel Lozano on 6/7/16.
+//  Copyright © 2016 Sergio Daniel L Garcia. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class func autosavesInPlace() -> Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        let windowController = storyboard.instantiateControllerWithIdentifier("Document Window Controller") as! NSWindowController
+        self.addWindowController(windowController)
+    }
+
+}
